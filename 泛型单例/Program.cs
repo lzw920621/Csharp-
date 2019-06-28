9155c17d8ea0b1ccdaee3d2e70821049c18a44f1
lzw920621 <1010616217@qq.com>
2019-06-28 10:30:36 +0800

    泛基和泛型单例

---
+++ 泛型单例/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泛型单例
+{
+    //不支持非公共的无参构造函数的   不推荐 因为T有公共的无参构造函数,可以在外部实例化T
+    public abstract class BaseInstance<T> where T : class, new()
+    {
+        private readonly static object lockObj = new object();
+        private static T instance = null;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObj)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new T();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+
+    //支持非公共的无参构造函数的
+    public class Singleton<T> where T : class
+    {
+        private static T instance;
+        private static object lockObject = new object();
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            CreateInstance();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }       
+        private static void CreateInstance()
+        {
+            Type t = typeof(T);
+            ConstructorInfo[] ctors = t.GetConstructors();
+            if (ctors.Length > 0)
+            {
+                throw new InvalidOperationException(String.Format("{0} has at least one accesible ctor making it impossibleto enforce DyhSingleton behaviour", t.Name));
+            }
+            instance = (T)Activator.CreateInstance(t, true);
+        }
+    }
+                    
+
+    public class ClassA
+    {
+
+    }
+
+    public class ClassB
+    {
+
+    }
+
+    public class ClassC:Singleton<ClassC>
+    {
+        private ClassC() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ClassA classASingleton1 = BaseInstance<ClassA>.Instance;
+            //ClassA classASingleton2 = BaseInstance<ClassA>.Instance;
+
+            //bool isEqual = classASingleton1.Equals(classASingleton2);
+
+            ClassC classC1 = ClassC.Instance;
+            ClassC classC2 = ClassC.Instance;
+            bool isEqual = classC1.Equals(classC2);
+        }
+    }
+}
---
+++ 泛基/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace 泛基
+{
+    public abstract class MyBase<TDerived> where TDerived : MyBase<TDerived>
+    {
+        public static List<string> GetMembersOfClassT()//获取子类成员信息
+        {
+            Type type = typeof(TDerived);
+            MemberInfo[] members = type.GetMembers();
+            List<string> membersList = new List<string>();
+            foreach (var item in members)
+            {
+                membersList.Add(item.Name);
+            }
+            return membersList;
+        }
+        public static List<string> GetFieldsOfClassT()//获取子类字段信息
+        {
+            Type type = typeof(TDerived);
+            FieldInfo[] fields = type.GetFields();
+            List<string> fieldList = new List<string>();
+            foreach (var item in fields)
+            {
+                fieldList.Add(item.Name);
+            }
+            return fieldList;
+        }
+        public static List<string> GetMethodsOfClassT()//获取子类方法信息
+        {
+            Type type = typeof(TDerived);
+            MethodInfo[] methods = type.GetMethods();
+            List<string> methodList = new List<string>();
+            foreach (var item in methods)
+            {
+                methodList.Add(item.Name);
+            }
+            return methodList;
+        }
+        public static TDerived InstanceOfT { get; protected set; }
+
+    }
+    class NewClass:MyBase<NewClass>
+    {
+        public int field1;
+        public int field2;
+        public void Method1()
+        {
+
+        }
+        public void Method2()
+        {
+
+        }
+        static NewClass()
+        {
+            InstanceOfT = new NewClass();
+        }        
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //可以通过父类来获取子类信息
+            MyBase<NewClass>.GetMembersOfClassT().ForEach((str) => Console.WriteLine(str));
+            Console.WriteLine("-----------------------------------------------");
+            MyBase<NewClass>.GetFieldsOfClassT().ForEach((str) => Console.WriteLine(str));
+            Console.WriteLine("-----------------------------------------------");
+            MyBase<NewClass>.GetMethodsOfClassT().ForEach((str) => Console.WriteLine(str));
+
+            //var myclass1 = MyBase<NewClass>.InstanceOfT;   //获取到的结果是null NewClass的静态构造函数未调用
+            //var myclass2 = MyBase<NewClass>.InstanceOfT;
+            //bool equal = myclass1.Equals(myclass2);
+            Console.ReadKey();
+        }
+    }
+}
