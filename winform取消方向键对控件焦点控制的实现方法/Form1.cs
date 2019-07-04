03a4395dbcd1cf5cb3e97a6dca8cb47fcb90b0b3
lzw920621 <1010616217@qq.com>
2019-07-04 13:38:48 +0800

    更新

---
+++ winform取消方向键对控件焦点控制的实现方法/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform取消方向键对控件焦点控制的实现方法
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)//取消方向键对控件的焦点的控件，用自己自定义的函数处理各个方向键的处理函数
+        {
+            switch (keyData)
+            {
+                case Keys.Up:
+                    //UpKey();
+                    return true;//不继续处理
+                case Keys.Down:
+                    //DownKey();
+                    return true;
+                case Keys.Left:
+                    //LeftKey();
+                    return true;
+                case Keys.Right:
+                    //RightKey();
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+    }
+}
---
+++ winform取消方向键对控件焦点控制的实现方法/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform取消方向键对控件焦点控制的实现方法
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
