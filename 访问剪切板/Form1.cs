ff1763be06ba8d6b17ee93beea13339eedf382b3
lzw920621 <1010616217@qq.com>
2019-07-18 11:10:21 +0800

    更新

---
+++ 访问剪切板/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 访问剪切板
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SetClipboardText(string str)
+        {
+            //方法1  (很可能有问题)
+            Clipboard.SetText(str);
+            //方法2  (很可能有问题)
+            Clipboard.SetData(DataFormats.Text, str);
+            //方法3  (大多数时候没问题)
+            Clipboard.SetDataObject(str);
+        }
+
+        public string GetClipboardText()
+        {
+            string str;
+
+            //方法1 (很可能有问题)
+            str = (string)Clipboard.GetData(DataFormats.Text);
+
+            //方法2 (大多数时候没问题)
+            str = Clipboard.GetText();
+
+            return str;
+        }
+    }
+}
