50010839ba21e3750b2071f32506a95d5294604b
lzw920621 <1010616217@qq.com>
2019-07-18 11:20:02 +0800

    更新

---
+++ 非独占延时/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 非独占延时
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void Delay(int milliSecond)
+        {
+            int start = Environment.TickCount;
+            while (Math.Abs(Environment.TickCount - start) < milliSecond)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Delay(2000);//界面不会卡死
+            button1.Text = "等待结束";
+        }
+    }
+}
