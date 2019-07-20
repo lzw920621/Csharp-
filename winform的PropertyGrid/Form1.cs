244113768d630c7bc80fbbcafa0ca20355f28ed8
lzw920621 <1010616217@qq.com>
2019-07-20 08:01:47 +0800

    更新

---
+++ winform的PropertyGrid/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform的PropertyGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Person personA = new Person();
+            
+            propertyGrid1.SelectedObject = personA;//这个控件可以将对象的属性列出来
+        }
+    }
+
+    public enum Gender
+    {
+        男,
+        女
+    }
+    class Person
+    {
+        [ReadOnly(true)]//在编辑器中只读,代码中赋值不受影响
+        [DisplayName("年龄")]//属性的显示名
+        public int Age { get; set; } = 15;
+
+        [ReadOnly(true)]
+        [DisplayName("姓名")]
+        public string Name { get; set; } = "tom";
+
+        [Browsable(false)]//是否可见
+        public Gender Sex { get; set; } = Gender.男;
+
+        
+    }
+}
---
+++ 利用反射实现浅拷贝/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace 利用反射实现浅拷贝
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person personClone = MyClone(person) as Person;
+        }
+
+        public static object MyClone(object obj)
+        {
+            Type type = obj.GetType();
+            object instanceClone = Activator.CreateInstance(type);//创建该类型的一个对象(使用的是默认的无参构造函数)
+            foreach (var property in type.GetProperties())
+            {
+                if(property.CanRead && property.CanWrite)
+                {
+                    object value = property.GetValue(obj);//获取要拷贝对象obj的该属性的值
+                    property.SetValue(instanceClone, value);//将该值赋值给新的实例instanceClone对应的属性
+                } //这里只对属性值进行了拷贝,如果有其他成员 比如 字段 也可以用类似的方法进行拷贝               
+            }
+            return instanceClone;//返回新的对象
+        }
+    }
+
+    public enum Gender
+    {
+        男,
+        女
+    }
+    class Person
+    {
+        public int Age { get; set; } = 15;
+        public string Name { get; set; } = "tom";
+        public Gender Sex { get; set; } = Gender.男;
+
+
+    }
+}
