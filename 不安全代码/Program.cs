0cb84848452a4a589324eadff597bc8b434f434a
lzw920621 <1010616217@qq.com>
2019-07-22 10:20:56 +0800

    更新

--- 不安全代码/Program.cs
+++ 不安全代码/Program.cs
@@ -10,7 +10,41 @@ namespace 不安全代码
     {
         static unsafe void Main(string[] args)
         {
+            //在同一个声明中声明多个指针时，星号 * 仅与基础类型一起写入；而不是用作每个指针名称的前缀。 例如:
+            //int* p1, p2, p3;     // 正确  
+            //int* p1, *p2, *p3;   // 错误 
 
+            //int num1 = 10;
+            //int* p1 = &num1;
+            ////*p1 = 20;
+            //Console.WriteLine((int)p1);
+            //Console.WriteLine(*p1);// *p1 是 p1指向的对象
+            //Console.WriteLine(p1->ToString());//p1->ToString() 是调用的p指向的对象的ToString方法
+            //Console.ReadKey();
+
+
+            int[] list = { 10, 100, 200 };
+
+            //fixed关键字的作用:固定变量位置. 
+            //因为GC在回收垃圾的时候会移动堆中对象的位置(碎片整理),如果一个变量的内存地址会变化，那么指针也就没有意义了
+            //所以要固定ptr的位置
+            fixed (int* ptr = list) //ptr为list[0]的地址                                   
+            {                
+                Console.WriteLine(*ptr);
+                /* 显示指针中数组地址 */
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Address of list[{0}]={1}", i, (int)(ptr + i));//ptr+i为list[i]的地址
+                    Console.WriteLine("Value of list[{0}]={1}", i, *(ptr + i));//*(ptr+i)为list[i]的值
+                }
+            }
+
+            //在unsafe不安全环境中，我们可以通过stackalloc在堆栈上分配内存
+            //其在堆栈上分配的内存不受内存管理器管理，因此其相应的指针不需要固定。
+            //该内存块的生存期受限于定义它的方法的生存期。 不能在方法返回之前释放内存
+            //int* ptr2 = stackalloc int[1];
+
+            Console.ReadKey();
         }
     }
 }
---
+++ 关于结构实现接口的一些问题/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace 关于结构实现接口的一些问题
+{
+
+    //在结构使用时，结构可以实现接口。这时就有一个问题“结构体实现接口后是值类型还是引用类型？”
+
+    struct StructClass : IClass
+    {
+        public int Count;//人数   
+        public void AddStudent()
+        {
+            Count++;
+        }
+
+        public void ShowCount()
+        {
+            Console.WriteLine(Count);
+        }
+
+    }
+    //接口   
+    interface IClass
+    {
+        void AddStudent();//添加学生   
+        void ShowCount();//显示学生人数   
+    }   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StructClass s1 = new StructClass();
+            StructClass s2 = s1;
+            s1.AddStudent();
+            s1.ShowCount(); //输出1   
+            s2.ShowCount(); //输出0   
+            //说明s2和s1不指向同一个对象，s2=s1是创建了一个s1的副本   
+            //这是值类型很显著的标志   
+
+            IClass ic1 = new StructClass();
+            IClass ic2 = ic1;
+            ic1.AddStudent();
+            ic1.ShowCount();//输出1   
+            ic2.ShowCount();//输出1   
+            //说明s2和s1指向同一个对象，s2=s1是将s1的引用赋给s2   
+            //这是引用类型很显著的标志   
+
+            Type type = typeof(StructClass);
+            Type type2 = typeof(IClass);
+            MethodInfo[] methodInfos = type.GetMethods();
+            FieldInfo[] fieldInfos = type.GetFields();
+            /*
+            最后得出结论：
+            当我们声明对象(s1、s2)是结构体类型时，对象是值类型，对象在栈中创建
+            当我们声明对象(ic1、ic2)是接口类型时，对象是引用类型，对象在堆中创建
+            */
+        }
+    }
+}
