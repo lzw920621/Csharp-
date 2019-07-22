f63de74394082a0fa83e150a0a0a14565143b54c
lzw920621 <1010616217@qq.com>
2019-07-22 15:21:21 +0800

    更新

---
+++ ORM框架之EntityFramework/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM框架之EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1;
+            using(EntityContext db = new EntityContext())
+            {
+                //var product = new Product() { Name = "电磁炉1", Description = "电器" };
+                //db.Set<Product>().Add(product);//添加元素
+
+                //product1 = db.Set<Product>().Find(1);//根据主键查找
+                //db.Set<Product>().Remove(product1);//删除元素
+                
+                int count = db.Set<Product>().Count();
+                db.SaveChanges();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class EntityContext : DbContext
+    {
+        public EntityContext()
+            : base("name=DBConnectionString")
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //不使用modelBuilder.Configurations.AddFromAssembly方法则需要逐个添加,如果数量多的话比较麻烦
+            //modelBuilder.Configurations.Add(new OneToMany.Map.ProductMap());
+            //此方法可以将当前程序集下所有继承了ComplexTypeConfiguration、EntityTypeConfiguration类型的类添加到注册器
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class ProductMap : EntityTypeConfiguration<Product>
+    {
+        public ProductMap()
+        {
+            ToTable("Product"); //指定表明
+            HasKey(p => p.Id);  //指定主键
+            //指定Name字段在数据库中列名为DBName、长度500、非空,其他的话可以自己试下
+            Property(p => p.Name).HasColumnName("DBName").HasMaxLength(500).IsRequired();
+        }
+    }
+}
--- ORM框架之EntityFramework/Properties/AssemblyInfo.cs
+++ ORM框架之EntityFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("EntityFrameworkStudy")]
+[assembly: AssemblyTitle("ORM框架之EntityFramework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EntityFrameworkStudy")]
+[assembly: AssemblyProduct("ORM框架之EntityFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("22191f14-1cb9-4f17-96f3-6e98d4353f08")]
+[assembly: Guid("3d6653e3-2c4c-4411-a99d-2b8769fcb116")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
