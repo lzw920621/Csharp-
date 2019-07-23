29aa05b335b1c0f1f5c10e9968c0b0fe1d8c5c74
lzw920621 <1010616217@qq.com>
2019-07-23 10:30:22 +0800

    更新

---
+++ ORM框架之Dapper/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+
+
+namespace ORM框架之Dapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            using(var db = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServerConnString"].ConnectionString))
+            {
+                //string sql = "INSERT INTO persons(ID,Name) Values (1,'张三');";
+                //int affectedRows = db.Execute(sql);
+
+                string query = "select * from persons";
+                List<Person> lstContact = db.Query<Person>(query).ToList<Person>();
+
+                foreach (var item in lstContact)
+                {
+                    Console.WriteLine(item.ID + item.Name + " " + item.Age + "\n");
+                }
+
+
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ORM框架之SqlSugar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM框架之SqlSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+        }
+    }
+}
