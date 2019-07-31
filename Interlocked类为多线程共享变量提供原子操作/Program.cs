e1258efc99cb10dc4dc3f72b78b5909e59c2162f
lzw920621 <1010616217@qq.com>
2019-07-31 16:13:02 +0800

    更新

---
+++ Interlocked类为多线程共享变量提供原子操作/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Interlocked类为多线程共享变量提供原子操作
+{
+    class Program
+    {
+        static int num1 = 0;
+        static int num2 = 0;
+        static int num3 = 0;
+        static readonly object lockObj = new object();
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 1000000, i =>
+            {
+                Interlocked.Increment(ref num1);
+            });
+            Parallel.For(0, 1000000, i =>
+              {
+                  num2++;//直接使用count++,  如果两个线程并行执行时, 两个线程中的一个的结果会被覆掉, 非线程安全.
+              });
+            Parallel.For(0, 1000000, i =>
+            {
+                lock(lockObj)
+                {
+                    num3++;
+                }
+            });
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.ReadKey();
+        }
+    }
+}
