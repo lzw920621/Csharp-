015ea202f91d71ded52b2c7a379c454405fb53ae
lzw920621 <1010616217@qq.com>
2019-08-17 19:55:04 +0800

    更新

--- 使用Process类打开文件或系统应用/Program.cs
+++ 使用Process类打开文件或系统应用/Program.cs
@@ -14,7 +14,7 @@ namespace 使用Process类打开文件或系统应用
             //Process.Start("notePad");//打开记事本
             //Process.Start("msPaint");//打开画图工具
             //Process.Start("calc");//打开计算器
-            string path = @"C:\Users\lzw92\Desktop\耳机组件半成品数据格式.csv";
+            string path = @"C:\Users\lzw92\Desktop\耳机组件半成品数据格式.csv";//文件路径
             Process.Start(path);
         }
     }
---
+++ 结构体使用属性时遇到的坑/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 结构体使用属性时遇到的坑
+{
+    class Program
+    {
+        public struct StructA
+        {
+            public int fieldA;
+            public int propertyB { get; set; }
+        }
+
+        public struct StructB
+        {
+            public int fieldA;
+            public int propertyB { get { return 0; } set { } }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{nameof(StructA)} size is: {Marshal.SizeOf(typeof(StructA))}");//长度为8
+            Console.WriteLine($"{nameof(StructB)} size is: {Marshal.SizeOf(typeof(StructB))}");//长度为4
+            
+            Console.ReadLine();
+        }
+    }
+}
