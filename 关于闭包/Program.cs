748da37606730e832b502d9b1a8a1c337b4868e4
lzw920621 <1010616217@qq.com>
2019-08-17 16:39:29 +0800

    更新

---
+++ 关于闭包/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 关于闭包
+{
+    //闭包是指有权访问另一个函数作用域中的变量的函数。
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetClosureFunction()(30));//依次输出:20 40 60
+           
+        }
+
+        static Func<int, int> GetClosureFunction()
+        {
+            int val = 10;
+            Func<int, int> internalAdd = x => x + val;
+
+            Console.WriteLine(internalAdd(10));
+
+            val = 30;
+            Console.WriteLine(internalAdd(10));
+
+            return internalAdd;
+        }
+        
+    }
+}
