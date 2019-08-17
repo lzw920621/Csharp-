8ae150e571eb4d5a577d1c58ef2b4986a24f101f
lzw920621 <1010616217@qq.com>
2019-08-17 18:09:01 +0800

    更新

---
+++ 闭包中的陷阱/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 闭包中的陷阱
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            //情形1
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    Thread.Sleep(1000);                    
+                    Console.WriteLine(i);
+                });
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("----------------------");
+            //情形2
+            int[] array = new int[] { 0, 1, 2, 3, 4, 5 };
+            foreach (var i in array)
+            {
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine(i);
+                });
+            }
+            Console.ReadKey();
+        }
+    }
+}
