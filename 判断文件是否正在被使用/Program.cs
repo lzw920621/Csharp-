4f5bf9248c51aa149e799288f71f48d213604595
lzw920621 <1010616217@qq.com>
2019-08-28 10:29:42 +0800

    更新

---
+++ 判断文件是否正在被使用/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 判断文件是否正在被使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool FileIsUsed(string fileFullName)
+        {
+            bool result = false;
+
+            //判断文件是否存在，如果不存在，直接返回 false
+            if (!System.IO.File.Exists(fileFullName))
+            {
+                result = false;
+
+            }//end: 如果文件不存在的处理逻辑
+            else
+            {//如果文件存在，则继续判断文件是否已被其它程序使用
+
+                //逻辑：尝试执行打开文件的操作，如果文件已经被其它程序使用，则打开失败，抛出异常，根据此类异常可以判断文件是否已被其它程序使用。
+                System.IO.FileStream fileStream = null;
+                try
+                {
+                    fileStream = System.IO.File.Open(fileFullName, System.IO.FileMode.Open, System.IO.FileAccess.ReadWrite, System.IO.FileShare.None);
+
+                    result = false;
+                }
+                catch (System.IO.IOException ioEx)
+                {
+                    result = true;
+                }
+                catch (System.Exception ex)
+                {
+                    result = true;
+                }
+                finally
+                {
+                    if (fileStream != null)
+                    {
+                        fileStream.Close();
+                    }
+                }
+
+            }
+            
+            return result;
+        }
+    }
+}
