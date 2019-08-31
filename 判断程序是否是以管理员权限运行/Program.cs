b28ef352a67a248e6e218bf0257575ea38d66bf5
lzw920621 <1010616217@qq.com>
2019-08-31 10:21:30 +0800

    更新

---
+++ 判断程序是否是以管理员权限运行/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace 判断程序是否是以管理员权限运行
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdministrator = IsAdministrator();
+        }
+
+        public static bool IsAdministrator()
+        {
+            WindowsIdentity current = WindowsIdentity.GetCurrent();
+            WindowsPrincipal windowsPrincipal = new WindowsPrincipal(current);
+            return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
