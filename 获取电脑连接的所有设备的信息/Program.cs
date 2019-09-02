d27626c9b66273e725cb6ffc7d7345a5110bf62a
lzw920621 <1010616217@qq.com>
2019-09-02 09:47:13 +0800

    更新

---
+++ 获取电脑连接的所有设备的信息/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 获取电脑连接的所有设备的信息
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = HardwareInfo.MulGetHardwareInfo(HardwareEnum.Win32_PnPEntity, "Name");
+        }
+    }
+}
