7c83d8138f1947881e63c54243395eb7ba264249
lzw920621 <1010616217@qq.com>
2019-09-02 19:32:21 +0800

    更新

---
+++ 以不同的进制输出数字/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 以不同的进制输出数字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 16;
+            //输出二进制形式
+            Console.WriteLine("0b"+Convert.ToString(num, 2));
+            //输出八进制形式
+            Console.WriteLine("0" + Convert.ToString(num, 8));
+            //输出十进制形式
+            Console.WriteLine(Convert.ToString(num, 10));
+            //输出十六进制形式
+            Console.WriteLine("0x" + Convert.ToString(num, 16));
+
+            Console.ReadKey();
+        }
+    }
+}
