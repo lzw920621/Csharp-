bba7f7115fbbeee0f989b2ac91426f6d52864d40
lzw920621 <1010616217@qq.com>
2019-09-03 11:15:03 +0800

    更新

---
+++ 在switch块中使用goto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 在switch块中使用goto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            switch(n)
+            {
+                case 1:goto default;
+                case 2:goto case 4;
+                case 3:break;
+                case 4:break;
+                case 5:break;
+                case 6:break;
+                case 7:break;
+                default:break;
+            }
+
+        }
+    }
+}
