c8690e9841cb9677cc99037e0d1d630444591694
lzw920621 <1010616217@qq.com>
2019-09-03 10:42:02 +0800

    更新

---
+++ 使用goto跳出多层循环/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 使用goto跳出多层循环
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    if(j==10)
+                    {
+                        goto QuitLoop;
+                    }
+                }
+            }
+            QuitLoop:
+            Console.ReadKey();
+            
+        }
+    }
+}
