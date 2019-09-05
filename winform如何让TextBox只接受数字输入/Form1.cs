c4166ace8b1953c6c3eca8bf88b04cce4896e12a
lzw920621 <1010616217@qq.com>
2019-09-05 18:36:27 +0800

    更新

---
+++ winform如何让TextBox只接受数字输入/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform如何让TextBox只接受数字输入
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            NumberOnlyTextBox numberOnlyTextBox = new NumberOnlyTextBox();
+            numberOnlyTextBox.Location = new System.Drawing.Point(298, 95);
+            numberOnlyTextBox.Name = "textBox1";
+            numberOnlyTextBox.Size = new System.Drawing.Size(187, 25);
+            numberOnlyTextBox.TabIndex = 0;
+            numberOnlyTextBox.Update();
+            numberOnlyTextBox.Text = "123";
+            this.Controls.Add(numberOnlyTextBox);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ winform如何让TextBox只接受数字输入/NumberOnlyTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform如何让TextBox只接受数字输入
+{
+    class NumberOnlyTextBox:TextBox
+    {
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+
+            base.OnKeyPress(e);
+
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar)))
+
+                e.Handled = true;
+        }
+    }
+}
