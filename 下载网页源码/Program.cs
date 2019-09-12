d48649f56146a065422c2648102c7af02c3a0f08
lzw920621 <1010616217@qq.com>
2019-09-12 14:24:02 +0800

    更新

---
+++ 下载网页源码/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 下载网页源码
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = DownloadCode("https://www.cnblogs.com/yinmu/p/10994088.html");
+
+        }
+
+        /// <summary>
+        /// 下载网页源代码
+        /// </summary>
+        /// <param name="Url">网页路径</param>
+        /// <returns></returns>
+        private static string DownloadCode(string Url)
+        {
+            string page = "";
+            try
+            {
+                WebClient webClient = new WebClient();
+                //webClient.Encoding = Encoding.UTF8;//否则很可能出现乱码
+                //page = webClient.DownloadString(Url);
+                byte[] pageData = webClient.DownloadData(Url);
+                page= Encoding.GetEncoding("utf-8").GetString(pageData);
+            }
+            catch (Exception ec)
+            {
+                throw new Exception(ec.Message.ToString());
+            }
+            return page;
+        }
+
+    }
+}
