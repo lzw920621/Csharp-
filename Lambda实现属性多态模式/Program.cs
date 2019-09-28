be0054411b5579b1e86161c9a88181d1fe4e1459
lzw920621 <1010616217@qq.com>
2019-09-28 09:01:31 +0800

    更新

---
+++ Lambda实现属性多态模式/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda实现属性多态模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class MyBaseClass
+    {
+        public Action SomeAction { get; protected set; }
+
+        public MyBaseClass()
+        {
+            SomeAction = () =>
+            {
+                //Do something!
+            };
+        }
+    }
+    class MyInheritedClass : MyBaseClass
+    {
+        public MyInheritedClass()
+        {
+            base.SomeAction = () =>
+            {
+                //Do something different!
+            };
+        }
+    }
+}
