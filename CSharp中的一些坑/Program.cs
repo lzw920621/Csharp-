34cf5d54267e84b5de3da002af256fcbcebbd897
lzw920621 <1010616217@qq.com>
2019-09-28 11:20:17 +0800

    更新

---
+++ CSharp中的一些坑/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp中的一些坑
+{
+    class Program
+    {
+        struct Point
+        {
+            public int x;
+            public int y;
+        }
+        class PointBox
+        {
+            public int Number { get; set; }
+            public Point Point { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            var ps1 = new Point[] { new Point { x = 1, y = 2 } };
+            ps1[0].x = 3;
+
+            var ps2 = new List<Point> { new Point { x = 1, y = 2 } };
+            ps2[0].x = 3;
+
+            var box = new PointBox() { Number = 1, Point = new Point { x = 1, y = 2 } };
+            box.Number += 3;
+            box.Point.x = 5;
+        }
+    }
+}
