1b951829d8509d6d18beadc1eb7a3c86b2d860ce
lzw920621 <1010616217@qq.com>
2019-10-09 10:42:56 +0800

    更新

---
+++ 获取本机IP地址/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace 获取本机IP地址
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = new Program().GetIpAddress();
+        }
+
+
+        private string GetIpAddress()
+        {
+            string hostName = Dns.GetHostName();   //获取本机名
+            IPHostEntry localhost = Dns.GetHostByName(hostName);    //方法已过期，可以获取IPv4的地址
+            //IPHostEntry localhost = Dns.GetHostEntry(hostName);   //获取IPv6地址
+            IPAddress localaddr = localhost.AddressList[0];
+
+            return localaddr.ToString();
+        }
+    }
+
+    
+}
