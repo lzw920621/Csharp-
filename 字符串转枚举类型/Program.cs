a6e8c2d90f60e1aff4e04196b76424e6366d19d1
lzw920621 <1010616217@qq.com>
2019-10-12 16:49:04 +0800

    更新

---
+++ 字符串转枚举类型/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 字符串转枚举类型
+{
+    enum MyEnum
+    {
+        type1,
+        type2,
+        type3,
+        type4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "type2";
+            MyEnum myEnum = (MyEnum)Enum.Parse(typeof(MyEnum), str);
+        }
+    }
+}
