7f6c7b03100b136037e4467aadb3373670050150
lzw920621 <1010616217@qq.com>
2019-10-15 15:20:27 +0800

    更新

---
+++ 为枚举类型添加描述方法/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 为枚举类型添加描述方法
+{
+    public static class EnumExtension
+    {
+        public static string GetDescription(this Enum val)
+        {
+            var type = val.GetType();
+            var memberInfo = type.GetMember(val.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if(attributes==null || attributes.Length==0)
+            {
+                return val.ToString();
+            }
+
+            return (attributes.Single() as DescriptionAttribute).Description;
+        }
+    }
+
+    public enum Level
+    {
+        [Description("Bad")]
+        B,
+        [Description("Normal")]
+        N,
+        [Description("Good")]
+        G,
+        [Description("Very Good")]
+        VG
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level level = Level.B;
+            string description=level.GetDescription();
+        }
+    }
+}
