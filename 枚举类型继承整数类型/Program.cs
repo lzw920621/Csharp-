10524ae3ab03427f66c2bb73f064707c3ba0a12c
lzw920621 <1010616217@qq.com>
2019-10-17 09:56:02 +0800

    更新

---
+++ 枚举类型继承整数类型/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 枚举类型继承整数类型
+{
+    enum Season:byte
+    {
+        春,
+        夏,
+        秋,
+        冬
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
