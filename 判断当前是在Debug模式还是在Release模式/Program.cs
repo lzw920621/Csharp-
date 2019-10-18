c2441c1babc80ad523c25173109a29f0ba237c77
lzw920621 <1010616217@qq.com>
2019-10-18 15:17:30 +0800

    更新

---
+++ 判断当前是在Debug模式还是在Release模式/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 判断当前是在Debug模式还是在Release模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if (DEBUG)
+            Console.Write("DEBUG下运行");
+#else
+            Console.Write("release下运行");
+#endif
+            Console.ReadKey();
+        }
+    }
+}
