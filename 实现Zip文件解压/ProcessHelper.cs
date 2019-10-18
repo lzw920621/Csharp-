8d0ca8372e3130a2b360848b977dce405a40d94f
lzw920621 <1010616217@qq.com>
2019-10-18 14:05:25 +0800

    更新

---
+++ 实现Zip文件解压/ProcessHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实现Zip文件解压
+{
+    class ProcessHelper
+    {
+        //方式1
+        /// <summary>
+        /// 解压
+        /// </summary>
+        /// <param name="zipExe">7-Zip工具的运行程序的路径</param>
+        /// <param name="zipFileName">待解压文件路径</param>
+        /// <param name="unZipPath">解压后文件存放目录</param>
+        /// <param name="pwd">解压密码</param>
+        public static void Unzip(string zipExe, string zipFileName, string unZipPath, string pwd)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = zipExe;   //7-Zip工具的运行程序
+            process.StartInfo.Arguments = String.Format("e \"{0}\" -p{1} -o\"{2}\"", zipFileName, pwd, unZipPath);
+            process.Start();
+        }
+
+        //方式2
+        public static string[] ExecCommand(string commands)
+        {
+            //msg[0]执行结果;msg[1]错误结果
+            string[] msg = new string[2];
+            Process proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "cmd.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+
+                proc.StandardInput.WriteLine(commands);
+                proc.StandardInput.WriteLine("exit");
+
+                //执行结果
+                msg[0] = proc.StandardOutput.ReadToEnd();
+                proc.StandardOutput.Close();
+
+                //出错结果
+                msg[1] = proc.StandardError.ReadToEnd();
+                proc.StandardError.Close();
+
+                //超时等待
+                int maxWaitCount = 10;
+                while (proc.HasExited == false && maxWaitCount > 0)
+                {
+                    proc.WaitForExit(1000);
+                    maxWaitCount--;
+                }
+                if (maxWaitCount == 0)
+                {
+                    msg[1] = "操作执行超时";
+                    proc.Kill();
+                }
+                return msg;
+            }
+            catch (Exception ex)
+            {
+                msg[1] = "进程创建失败:";
+                msg[1] += ex.Message.ToString();
+                msg[1] += ex.StackTrace.ToString();
+            }
+            finally
+            {
+                proc.Close();
+                proc.Dispose();
+            }
+            return msg;
+        }
+
+    }
+}
---
+++ 实现Zip文件解压/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实现Zip文件解压
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipExe = @"C:\Program Files\7-Zip\7z.exe";   //7-Zip工具的运行程序
+            string zipFileName = @"E:\MyZip\Report.zip";        //需要被解压的Zip文件
+            string unZipPath = @"E:\MyZip\";                    //解压后文件存放目录
+            string pwd = "123";                                 //解压密码
+
+            //执行解压命令
+            string cmd = String.Format("\"{0}\" e \"{1}\" -p{2} -o\"{3}\"", zipExe, zipFileName, pwd, unZipPath);
+            ProcessHelper.ExecCommand(cmd);
+        }
+        
+    }
+}
