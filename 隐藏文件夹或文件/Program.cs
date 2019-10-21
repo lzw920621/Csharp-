4ec61189b6512074b3f2d5cefbe32d8127ce14d9
lzw920621 <1010616217@qq.com>
2019-10-21 18:09:18 +0800

    更新

---
+++ 隐藏文件夹或文件/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 隐藏文件夹或文件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string desDir = @"C:\Users\lzw92\Desktop\新建文件夹 (6)";
+            DirectoryInfo dirInfo = new DirectoryInfo(desDir);
+            dirInfo.Attributes = FileAttributes.Hidden;//隐藏文件夹
+            //dirInfo.Attributes = FileAttributes.Directory;//还原
+            
+
+            //FileInfo dirInfo = new FileInfo(desDir);
+            //dirInfo.Attributes = FileAttributes.Hidden;//隐藏文件
+        }
+    }
+}
