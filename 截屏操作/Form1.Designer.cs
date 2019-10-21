855f7df42e1f777535919cda4b05107675bedbf4
lzw920621 <1010616217@qq.com>
2019-10-21 17:54:23 +0800

    更新

---
+++ 截屏操作/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace 截屏操作
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_ScreenShoot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_ScreenShoot
+            // 
+            this.button_ScreenShoot.Location = new System.Drawing.Point(281, 102);
+            this.button_ScreenShoot.Name = "button_ScreenShoot";
+            this.button_ScreenShoot.Size = new System.Drawing.Size(181, 63);
+            this.button_ScreenShoot.TabIndex = 0;
+            this.button_ScreenShoot.Text = "截屏";
+            this.button_ScreenShoot.UseVisualStyleBackColor = true;
+            this.button_ScreenShoot.Click += new System.EventHandler(this.button_ScreenShoot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 276);
+            this.Controls.Add(this.button_ScreenShoot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_ScreenShoot;
+    }
+}
+
---
+++ 截屏操作/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 截屏操作
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 截全屏并保存成图片
+        /// </summary>
+        void getScreen()
+        {
+            Image myImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);//截取整个屏幕
+            //Image myImage = new Bitmap(Screen.PrimaryScreen.WorkingArea.Right, Screen.PrimaryScreen.WorkingArea.Bottom);//截取整个工作区
+
+            Graphics g = Graphics.FromImage(myImage);
+
+            g.CopyFromScreen(new Point(0, 0), new Point(0, 0), new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+            g.ReleaseHdc(g.GetHdc());
+
+            myImage.Save(@"C:\Users\lzw92\Desktop\新建文件夹 (6)\b.jpg");
+        }
+
+        private void button_ScreenShoot_Click(object sender, EventArgs e)
+        {
+            getScreen();
+        }
+       
+    }
+
+    
+}
