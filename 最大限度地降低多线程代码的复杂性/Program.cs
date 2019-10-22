88c4687d87f94d6d07fb3d2071fb6692d5cab4fd
lzw920621 <1010616217@qq.com>
2019-10-22 23:16:02 +0800

    更新

---
+++ 最大限度地降低多线程代码的复杂性/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 最大限度地降低多线程代码的复杂性
+{
+    interface IReadFromShared
+    {
+        string GetValue();
+    }
+
+    interface IWriteToShared
+    {
+        void SetValue(string value);
+    }
+
+    class MySharedClass : IReadFromShared, IWriteToShared
+    {
+        string _foo;
+
+        public string GetValue()
+        {
+            return _foo;
+        }
+
+        public void SetValue(string value)
+        {
+            _foo = value;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        void Foo(Synchronizer<MySharedClass, IReadFromShared, IWriteToShared> sync)
+        {
+            sync.Write(x =>
+            {
+                x.SetValue("new value");
+            });
+            sync.Read(x =>
+            {
+                Console.WriteLine(x.GetValue());
+            });
+        }
+    }
+}
---
+++ 最大限度地降低多线程代码的复杂性/Synchronizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 最大限度地降低多线程代码的复杂性
+{
+    class Synchronizer<TImpl, TIRead, TIWrite> where TImpl : TIWrite, TIRead
+    {
+        ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        TImpl _shared;
+
+        public Synchronizer(TImpl shared)
+        {
+            _shared = shared;
+        }
+
+        public void Read(Action<TIRead> functor)
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                functor(_shared);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public void Write(Action<TIWrite> functor)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                functor(_shared);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
