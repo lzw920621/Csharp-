e8814a8a8e8abd409a2b62d0bfdf714fad0acae9
lzw920621 <1010616217@qq.com>
2019-10-22 08:26:14 +0800

    更新

---
+++ 使用迭代器输出斐波那契数列/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 使用迭代器输出斐波那契数列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in Fibonacci(10))
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<int> Fibonacci(int number)
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 0; i < number; i++)
+            {
+                yield return b;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
