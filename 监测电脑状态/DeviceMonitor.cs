a8bc935025a2da1b2b1c803e4907951317547e33
lzw920621 <1010616217@qq.com>
2019-10-29 09:51:34 +0800

    更新

---
+++ 监测电脑状态/DeviceMonitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 监测电脑状态
+{
+    class DeviceMonitor
+    {
+        static readonly PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        static readonly PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        static readonly PerformanceCounter uptime = new PerformanceCounter("System", "System Up Time");
+
+
+        public static bool GetInternetAvilable()
+        {
+            bool networkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+            return networkUp;
+        }
+
+        public static TimeSpan GetSystemUpTime()
+        {
+            uptime.NextValue();
+            TimeSpan ts = TimeSpan.FromSeconds(uptime.NextValue());
+            return ts;
+        }
+
+        public static string GetPhysicalMemory()
+        {
+            string str = null;
+            ManagementObjectSearcher objCS = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            foreach (ManagementObject objMgmt in objCS.Get())
+            {
+                str = objMgmt["totalphysicalmemory"].ToString();
+            }
+            return str;
+        }
+
+        public static string getCurrentCpuUsage()
+        {
+            return cpuCounter.NextValue() + "%";
+        }
+
+        public static string getAvailableRAM()
+        {
+            return ramCounter.NextValue() + "MB";
+        }
+    }
+}
---
+++ 监测电脑状态/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 监测电脑状态
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("是否已联网:" + DeviceMonitor.GetInternetAvilable());
+            Console.WriteLine("系统运行时间:" + DeviceMonitor.GetSystemUpTime());
+            Console.WriteLine("物理内存大小:" + DeviceMonitor.GetPhysicalMemory());
+            Console.WriteLine("可用内存大小:" + DeviceMonitor.getAvailableRAM());
+            Console.WriteLine("CPU占用率:" + DeviceMonitor.getCurrentCpuUsage());
+
+            Console.ReadKey();
+        }
+    }
+}
