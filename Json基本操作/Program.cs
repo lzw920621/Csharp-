0b6723ba607bd9525cd908f77152492297dad527
lzw920621 <1010616217@qq.com>
2019-10-30 23:33:00 +0800

    更新

--- Json基本操作/Program.cs
+++ Json基本操作/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
 
 namespace Json基本操作
 {
@@ -10,6 +12,10 @@ namespace Json基本操作
     {
         static void Main(string[] args)
         {
+            string jsonData = File.ReadAllText(@"C:\Users\lzw92\Documents\WeChat Files\lzw920621\FileStorage\File\2019-10\38803MB9EU000FSE.20190522183554.json");
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            Dictionary<string, object> dic = (Dictionary<string, object>)serializer.DeserializeObject(jsonData);
+            
         }
     }
 }
