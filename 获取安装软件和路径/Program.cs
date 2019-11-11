9248ddaa6f39cd040edde98c35a1e155927606ac
lzw920621 <1010616217@qq.com>
2019-11-11 17:49:59 +0800

    更新

---
+++ 获取安装软件和路径/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 获取安装软件和路径
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall", false))
+            {
+                if (key != null)//判断对象存在
+                {
+                    foreach (string keyName in key.GetSubKeyNames())//遍历子项名称的字符串数组
+                    {
+                        using (RegistryKey key2 = key.OpenSubKey(keyName, false))//遍历子项节点
+                        {
+                            if (key2 != null)
+                            {
+                                string softwareName = key2.GetValue("DisplayName", "").ToString();//获取软件名
+                                string installLocation = key2.GetValue("InstallLocation", "").ToString();//获取安装路径
+                                if (!string.IsNullOrEmpty(installLocation))
+                                {
+                                    dic[softwareName] = installLocation;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
