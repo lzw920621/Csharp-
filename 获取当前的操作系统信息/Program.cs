d2e6540a6b44b65b8e104b119c84ccb65e158ee3
lzw920621 <1010616217@qq.com>
2019-11-16 19:39:21 +0800

    更新

---
+++ 获取当前的操作系统信息/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 获取当前的操作系统信息
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //获取系统信息
+            System.OperatingSystem osInfo = System.Environment.OSVersion;
+            //获取操作系统ID
+            System.PlatformID platformID = osInfo.Platform;
+            //获取主版本号
+            int versionMajor = osInfo.Version.Major;
+            //获取副版本号
+            int versionMinor = osInfo.Version.Minor;
+        }
+    }
+}
