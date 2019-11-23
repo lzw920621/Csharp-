57e4c6c7c33cae5312a3e8234d889fcc0a231435
lzw920621 <1010616217@qq.com>
2019-11-23 15:49:41 +0800

    更新

---
+++ 小技巧_给事件添加一个空委托以避免空检查/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 小技巧_给事件添加一个空方法以避免空检查
+{
+    class Program
+    {
+        //event Action DoSomething;
+
+        event Action DoSomething = delegate { };//添加一个空方法,避免每次调用事件时要判断是否为空
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            ////常规调用
+            //if(program!=null)
+            //{
+            //    program.DoSomething();
+            //}
+
+            //加了空方法后,直接调用 不用判断是否为空
+            program.DoSomething();
+        }
+    }
+}
