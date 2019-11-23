d5093b01cfdc85e516e3bebe4a623adf221b26d8
lzw920621 <1010616217@qq.com>
2019-11-23 15:00:32 +0800

    更新

---
+++ 子类调用父类静态成员/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 子类调用父类静态成员
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Base.num;
+            num = Derived.num;
+        }
+    }
+
+    class Base
+    {
+        public static int num = 10;
+    }
+
+    class Derived:Base
+    {
+
+    }
+}
