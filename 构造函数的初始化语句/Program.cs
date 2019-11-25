26a516cc28e16662c6fd55545557b96f3aea1426
lzw920621 <1010616217@qq.com>
2019-11-25 14:07:15 +0800

    更新

---
+++ 构造函数的初始化语句/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 构造函数的初始化语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class MyBaseClass
+    {
+        public MyBaseClass(int a,int b)
+        {
+
+        }
+    }
+
+    class MyDerivedClass:MyBaseClass
+    {
+        public MyDerivedClass(int c):base(c,c)
+        {
+
+        }
+        public MyDerivedClass(int a,int b,int c):base(a,b)
+        {
+
+        }
+    }
+}
