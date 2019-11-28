b6385d7640cda70f0b3e4cef8fedd715a152e067
lzw920621 <1010616217@qq.com>
2019-11-28 15:24:07 +0800

    更新

---
+++ 杀死进程和子进程/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 杀死进程和子进程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //博客原文:https://www.cnblogs.com/TianFang/archive/2010/05/19/1739614.html
+        static void KillProcessAndChildren(int pid)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                Console.WriteLine(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                /* process already exited */
+            }
+        }
+        
+    }
+}
