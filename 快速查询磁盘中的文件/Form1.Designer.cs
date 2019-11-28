016330c26a994008818a9b3bc25dab49586d702c
lzw920621 <1010616217@qq.com>
2019-11-28 16:15:31 +0800

    更新

---
+++ 快速查询磁盘中的文件/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace 快速查询磁盘中的文件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_File_List = new System.Windows.Forms.ListBox();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_Count = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox_File_List
+            // 
+            this.listBox_File_List.FormattingEnabled = true;
+            this.listBox_File_List.ItemHeight = 15;
+            this.listBox_File_List.Location = new System.Drawing.Point(54, 75);
+            this.listBox_File_List.Name = "listBox_File_List";
+            this.listBox_File_List.Size = new System.Drawing.Size(588, 334);
+            this.listBox_File_List.TabIndex = 0;
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(661, 35);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(109, 64);
+            this.button_Search.TabIndex = 1;
+            this.button_Search.Text = "查询";
+            this.button_Search.UseVisualStyleBackColor = true;
+            this.button_Search.Click += new System.EventHandler(this.button_Search_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(588, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox_Count
+            // 
+            this.textBox_Count.Location = new System.Drawing.Point(54, 416);
+            this.textBox_Count.Name = "textBox_Count";
+            this.textBox_Count.Size = new System.Drawing.Size(100, 25);
+            this.textBox_Count.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 467);
+            this.Controls.Add(this.textBox_Count);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.listBox_File_List);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_File_List;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox_Count;
+    }
+}
+
---
+++ 快速查询磁盘中的文件/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 快速查询磁盘中的文件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Search_Click(object sender, EventArgs e)
+        {            
+            bool IsAdministrator = MFTScanner.IsAdministrator();
+            if(IsAdministrator==false)
+            {
+                MessageBox.Show("当前不是管理员身份!!!");
+                return;
+            }
+            MFTScanner mFTScanner = new MFTScanner();
+            var files = mFTScanner.EnumerateFiles(@"I:\").ToArray();
+            for (int i = 0; i < 100 && i<files.Length; i++)//只显示前100个文件名
+            {
+                listBox_File_List.Items.Add(files[i]);
+            }
+            textBox_Count.Text = files.Length+"";
+        }
+    }
+}
