237590da49e35c544a5977727f59a5160871bd6c
lzw920621 <1010616217@qq.com>
2019-11-30 09:15:42 +0800

    更新

--- 尾递归求斐波拉契数列/Program.cs
+++ 尾递归求斐波拉契数列/Program.cs
@@ -11,13 +11,20 @@ namespace 尾递归求斐波拉契数列
         static void Main(string[] args)
         {
             int num = Fibonacci(10, 0, 1);
+            int num2 = Factorial(10);
         }
-
+        //尾递归求斐波那契数列
         static int Fibonacci(int n,int num1,int num2)
         {
             if (n == 0) return num1;
             return Fibonacci(n - 1, num2, num1 + num2);
         }
         
+        //尾递归求阶乘
+        static int Factorial(int n, int s=1,int num=2)
+        {
+            if (n == 1) return s;
+            return Factorial(n - 1, s * num, num + 1);
+        }
     }
 }
